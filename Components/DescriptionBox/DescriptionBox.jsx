082278d2c32ac902fc05c218b03b07cc45a004d5
Priyanshu Pandey <97188634+ippandey@users.noreply.github.com>
2024-05-07 00:03:33 +0530

    Add files via upload

---
+++ Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          An e-commerce is an online platform that facilitate buying and selling
+          of products or services over the internet serves as a birtual
+          marketplace where businesses and individual showcase their products,
+          interact with customers, and conduct transactions without the need for
+          a physical presence. E-commerce websites have gained immense
+          popularity due to their convenient accessbility, and the global reach
+          they offer.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+          voluptatibus eos quo odit quis eaque ex animi? Voluptatem tempora
+          labore temporibus perspiciatis quidem fuga dignissimos velit, atque
+          numquam praesentium eos perferendis cum quia quibusdam vero recusandae
+          esse minus nostrum laboriosam exercitationem quam tempore dolorum
+          repellat aliquam! Earum ullam quos, temporibus molestias iste totam
+          mollitia. Iusto, corporis voluptatibus. Adipisci facilis, quas
+          laudantium quasi dolor amet magni error quos beatae a et.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
